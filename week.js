3247c381bda2aa93fee33238311f899424d80ed8
preethi <preethi.natarajan@agiratech.com>
2019-11-19 14:38:17 +0530

    week updated with delete method

--- week.js
+++ week.js
@@ -1,28 +1,27 @@
-
-
-  var express = require('express');
-  var bodyParser = require('body-parser');
-  var foo = express();
-  var mysql = require('mysql');
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "preethi",
-    password: "Preethi@123",
-    database: "db"});
-
-  foo.use(bodyParser())
-  con.connect();
-  foo.use(bodyParser.urlencoded({ extended: true }));
-  foo.post("/db/v1", function(req, res) {
-  con.query(' INSERT INTO week(user_id,date,agenda,time,mode,status)   values ("'+req.body.user_id+'","'+req.body.date+'","'+req.body.agenda+'","'+req.body.time+'","'+req.body.mode+'","'+req.body.status+'")', function (err, res) {
+var express = require('express');
+var bodyParser = require('body-parser');
+var foo = express();
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "preethi",
+  password: "Preethi@123",
+  database: "db"});
+foo.use(bodyParser())
+con.connect();
+foo.use(bodyParser.urlencoded({ extended: true }));
+foo.delete("/db/v1", function(req, response) {
+  var sql = "DELETE from week WHERE user_id = ('"+req.body.user_id+"')";
+  con.query(sql, function (err, result) {
+  console.log("test");
   if(err){
-     res.status(400).send('error');
+    response.status(400).send('error');
     }else{
-    console.log("one item added");
+    console.log("id deleted");
     }
   });
 });
 
-  foo.listen(3002,function(){
+foo.listen(3002,function(){
     console.log("listening to 3002");
-  });
+});
